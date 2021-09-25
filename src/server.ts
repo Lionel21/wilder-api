75543cc2366333d5812289a1c40d976f376e81af
Lionel Delamare <lionel.delamare>
2021-09-25 17:55:12 +0200

    Create env variables

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 
+require('dotenv').config();
 const mongoose = require('mongoose');
 const WilderControl = require('./controllers/WilderController');
 
 // Mango Atlas URL
 const url = 'mongodb+srv://lionel:210592Li@wildermodel.vpg92.mongodb.net/WilderDatabase?retryWrites=true&w=majority';
 
-// Database connection
+// Database Connection
 const connectDB = async () => {
   await mongoose.connect(url, {
     autoIndex: true,
@@ -25,6 +26,6 @@ app.get('/api/wilder/retrieve/:id', WilderControl.retrieve);
 app.put('/api/wilder/update/:id', WilderControl.update);
 app.delete('/api/wilder/delete/:id', WilderControl.delete);
 
-// Start Server
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// Running Server
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server running on port ${port}`));
